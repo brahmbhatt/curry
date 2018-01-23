d5db352cee1ae36913605c725df61ba5e1a3e6e4
brahmbhatt <margibrahmbhatt13@gmail.com>
2018-01-23 15:16:19 +0530

    first problem curyy1 added

---
+++ curry1.js
@@ -0,0 +1,5 @@
+function curry(arr)
+{
+  return arr;
+}
+module.exports = curry;
---
+++ curry1.test.js
@@ -0,0 +1,6 @@
+let curry = require('./curry1');
+describe('test case', () => {
+  test('case1:', () => {
+    expect(curry(3)).toBe(3);
+  });
+});
