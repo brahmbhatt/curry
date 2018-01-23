1421a42881c7d398367604b667e3c767c19a4aab
brahmbhatt <margibrahmbhatt13@gmail.com>
2018-01-23 15:31:54 +0530

    2nd  testcase for problem 2 added

--- curry2.test.js
+++ curry2.test.js
@@ -3,4 +3,8 @@ describe('testcases for problem 2:', () =>{
   test('arguments as numbers:', () =>{
     expect(curry(2,3)).toBe(5);
   });
+
+  test('arguments as numbers:', () =>{
+    expect(typeof(curry(2,3))).toBe('number');
+  });
 });
